7c1abaaf88a5cb1a69b21256fd5c74302a125df4
ariadna <agonzalez@itpatagonia.com>
2023-03-08 10:54:00 -0300

    login

--- servicios/google/login.go
+++ servicios/google/login.go
@@ -1,37 +1,41 @@
 package login
 
 import (
-	"context"
-
-	"github.com/gofiber/fiber"
-	"github.com/gofiber/fiber/v2"
 )
+//	"context"
+
+//	"github.com/gofiber/fiber"
+//	"github.com/gofiber/fiber/v2"
 
 type User struct {
-	Token    int    `json:"token"`
-	User     string `json:"user"`
-	Email    string `json:"email"`
-	Nombre   string `json:"nombre"`
-	Apellido string `json:"apellido"`
+	ClientId   string `json:"clientId"`
+	Credential string `json:"credential"`
 }
 
-func SignIn(c *fiber.Ctx, googleUser) error {
-	coll := googleUser.getBasicProfile("https://apis.google.com/js/platform.js").Content("226207107235-qiemrp4uajoakgvu2gi4obckqmgvdg9c.apps.googleusercontent.com")
-	profile := coll.Find(context.TODO())
+func SignIn(googleUser) error {
+	var profile := googleUser.getBasicProfile()
 
-	email := (("email") + profile.getEmail())
-	coll.Where("email = ?", profile.getEmail).First(&user)
-	if email.User == "" {
-		return c.SendString("usuario no habilitado")
-	}
-
-	nombre := (("nombre") + profile.getNombre())
-	apellido := (("apellido") + profile.getApellido())
-
-	user := (("user") + profile.getUser())
-
-	token = googleUser.getAuthResponse()
-	console.log(token)
-
-	return (profile)
 }
+
+//func SignIn(c *fiber.Ctx) error {
+
+//	coll := googleUser.getBasicProfile("https://apis.google.com/js/platform.js").Content("226207107235-qiemrp4uajoakgvu2gi4obckqmgvdg9c.apps.googleusercontent.com")
+//	profile := coll.Find(context.TODO())
+
+//	email := (("email") + profile.getEmail())
+//	coll.Where("email = ?", profile.getEmail).First(&email)
+//	if email.User == "" {
+//		return c.SendString("usuario no habilitado")
+//	}
+
+//	nombre := (("nombre") + profile.getNombre())
+//	apellido := (("apellido") + profile.getApellido())
+
+//	user := (("user") + profile.getUser())
+
+//	token = googleUser.getAuthResponse()
+//	console.log(token)
+//	console.log(token)
+
+//	return (profile)
+//}
