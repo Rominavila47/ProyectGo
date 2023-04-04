2a39c0cd2243b0cc9758cadd856bf2421f4c4f27
ariadna <agonzalez@itpatagonia.com>
2023-04-04 09:38:13 -0300

    cambios log

--- servicios/log/log.go
+++ servicios/log/log.go
@@ -1,27 +1,28 @@
 package log
 
-import (
-	"log"
-	"os"
-
-	"github.com/gofiber/fiber/v2"
+const (
+	Ldate = 1 << iota
+	Ltime
+	Llongfile
 )
 
-type Logger struct {
+func SetFlags(flag int) {
+
 }
 
-func Log(c *fiber.Ctx) error {
-	logger, err := os.OpenFile("log.txt", os.O_APPEND|os.O_CREATE|os.O_WRONLY, 0666)
-	if err != nil {
-		log.Fatal(err)
-	}
+//func Log(c *fiber.Ctx) error {
 
-	defer logger.Close()
+//	logger, err := os.OpenFile("log.txt", os.O_APPEND|os.O_CREATE|os.O_WRONLY, 0666)
+//	if err != nil {
+//		log.Fatal(err)
+//	}
 
-	log.SetOutput(logger)
+//	defer logger.Close()
 
-	for i := 0; i <= 10; i++ {
-		log.Println("error linea %v", i)
-	}
-	return (nil)
-}
+//	log.SetOutput(logger)
+
+//	for i := 0; i <= 10; i++ {
+//		log.Println("error linea %v", i)
+//	}
+//	return (nil)
+//}
